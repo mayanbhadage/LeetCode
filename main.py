07ea0567dd3c8fff2a54ea2c803616f6d69bc761
mayanbhadage <42819385+mayanbhadage@users.noreply.github.com>
2019-11-20 12:50:46 -0500

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def invertTree(self, root: TreeNode) -> TreeNode:
-        if root is not None:
-            root.left , root.right = root.right, root.left
-            self.invertTree(root.left)
-            self.invertTree(root.right)
-        return root
