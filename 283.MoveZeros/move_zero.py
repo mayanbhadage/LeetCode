369da97c73921eb5c8b5d190a7b49d50e963797f
Mayan Bhadage <mbhadag1@binghamton.edu>
2019-11-26 19:29:27 -0500

    26 Nov 2019

---
+++ 283.MoveZeros/move_zero.py
@@ -0,0 +1,17 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        count = 0
+        size = len(nums)
+        i = 0
+        while(i < size):
+            if nums[i] == 0:
+                count +=1
+                del nums[i]
+                size -= 1
+            else:
+                i+=1
+        for _ in range(count):
+            nums.append(0)
