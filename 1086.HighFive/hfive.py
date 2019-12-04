50b9f532743731d3570ff9a85a1d77075976f0ac
Mayan Bhadage <mbhadag1@binghamton.edu>
2019-12-03 22:02:24 -0500

    3 Dec 2019

--- 1086.HighFive/hfive.py
+++ 1086.HighFive/hfive.py
@@ -19,7 +19,7 @@ class Solution:
             res.append(hmap[key])
             result.append(res)
         
-        return result
+        return (result)
         
     def getAvg(self,total,num):
         
