a374b93396c44f2527fb30f46bb214acc8cb1dea
Mayan Bhadage <mbhadag1@binghamton.edu>
2019-12-04 17:01:15 -0500

    4 Dec 2019

--- 1221.SplitAStringInBalancedString/split.py
+++ 1221.SplitAStringInBalancedString/split.py
@@ -2,14 +2,8 @@ class Solution:
     def balancedStringSplit(self, s: str) -> int:
         r_count = 0
         l_count = 0
-        result = 0
-        first = s[0]
-        if first == "R":
-            r_count += 1
-        else:
-            l_count += 1
-        
-        for ch in s[1:]:
+        result = 0 
+        for ch in s:
             if ch is 'R':
                 r_count += 1
             else:
