005f1433061b23b03a2b48a7e0d210c4361e4fda
Mayan Bhadage <mbhadag1@binghamton.edu>
2020-03-31 12:58:16 -0400

    31 March 2020

--- 412.FizzBuzz/FizzBuzz.py
+++ 412.FizzBuzz/FizzBuzz.py
@@ -1,6 +1,19 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         result = []
+        for i in range(1,n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                result.append("FizzBuzz")
+            elif i % 3 == 0:
+                result.append("Fizz")
+            elif i % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(i))
+        return result
+        class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        result = []
         for num in range(1,n+1):
             if (num % 3 == 0 and num % 5== 0):
                 result.append("FizzBuzz")
