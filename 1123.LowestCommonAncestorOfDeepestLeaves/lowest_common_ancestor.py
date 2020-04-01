ec853fe8a173d486b173a6131bd300fb2ec7393a
Mayan Bhadage <mbhadag1@binghamton.edu>
2020-04-01 18:43:53 -0400

    1 April 2020

---
+++ 1123.LowestCommonAncestorOfDeepestLeaves/lowest_common_ancestor.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lcaDeepestLeaves(self, root: TreeNode) -> TreeNode:
+        if root is None:
+            return
+        
+        queue = deque()
+        parent = collections.defaultdict(list)
+        queue.append((root,0))
+        
+        while(queue):
+            curr_level = []
+            num_nodes = len(queue)
+            
+            for _ in range(num_nodes):
+                currNode, currHeight = queue.popleft()
+                curr_level.append(currNode)
+                if currNode.left:
+                    queue.append((currNode.left,currHeight+1))
+                    parent[currNode.left] = currNode
+                if currNode.right:
+                    queue.append((currNode.right,currHeight+1))
+                    parent[currNode.right] = currNode
+                    
+        
+        
+        # till we found the parent
+        while(len(curr_level) > 1):
+            s = set()
+            for node in curr_level:
+                s.add(parent[node])
+            curr_level = list(s)
+        
+        return curr_level[0]
+        
