a3817c73e9beafae245d93bfe85d73564cf2ed57
Mayan Bhadage <mbhadag1@binghamton.edu>
2020-11-01 14:44:00 -0500

    1 Nov 2020

--- 5.LongestPalindromicSubstring/longest_palindromic_substring.py
+++ 5.LongestPalindromicSubstring/longest_palindromic_substring.py
@@ -2,14 +2,17 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         res = ""
         for i in range(len(s)):
-            even = self.check(s, i, i)
-            odd = self.check(s, i , i+1)
+            odd = self.check(s, i, i)
+            even = self.check(s, i , i+1)
             
             res = max(res, even, odd, key=len)
         return res
         
         
-        
+   '''
+   The while loop stops either because l or r is out of range or because the s[l] != s[r],
+   which means neither s[l] nor s[r] can be part of the longest palindrome and the helper function returns s[l+1:r](inclusive on the left and exclusive on the right).
+   '''     
     def check(self,s, l, r):
         while(l >= 0 and r < len(s) and s[l] == s[r]):
             l -= 1
